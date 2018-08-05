bcd7a581ca54f9e7ddafcb6c6061a0342c8042aa
May <may1092200258@gmail.com>
2018-08-06 01:29:34 +0800

    UIHeart

---
+++ Assets/Script/UIHeartDisappear.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHeartDisappear : MonoBehaviour {
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void AnimControl()
+    {
+        anim.SetTrigger("Disappear");
+        Debug.Log("5");
+    }
+
+}
