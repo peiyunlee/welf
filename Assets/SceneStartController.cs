5f9456f505a87e2be19636761c3d0bf26f6109d6
May <may1092200258@gmail.com>
2018-10-20 21:20:46 +0800

    10/20 GUGU

---
+++ Assets/SceneStartController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneStartController : MonoBehaviour {
+    
+    protected void OnBtnStart()
+    {
+       // SceneManager.LoadScene(Home);
+    }
+}
