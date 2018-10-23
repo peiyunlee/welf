9b0a476d7169c2a7ad763748a51c8b3d555d6c4f
May <may1092200258@gmail.com>
2018-10-24 00:49:15 +0800

    10/24 gugu

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
+    //設定介面未做
     static GameManager instance;
-    public GameObject Setmenu; 
+    public GameObject Setmenu;
+    public GameObject Exitagree;
+
+    [SerializeField]
+    private bool issetmenu;
+    private bool isgameexit = false;
+
     void Awake()
     {
         if (instance == null)   //當第一個GameManager出現不要destroy
@@ -29,22 +36,28 @@ public class GameManager : MonoBehaviour {
         {
             LoadSetCanvas();
         }
+        else if (isgameexit)     //choose 確認退出 become true
+        {
+            Application.Quit();
+        }
+        else if (!isgameexit)   //choose 取消退出 become false 回到選單
+        {
+            Exitagree.SetActive(false);
+        }
     }
-
     private void LoadSetCanvas()  //出現設定介面
     {
         Setmenu.SetActive(true);
     }
 
-
-    //Button
+    /*//Button
     public void OnYesExitBtn() //確認退出
     {
         Application.Quit();
     }
-
     public void OnNoExitBtn() //取消退出
     {
         //回到設定介面
     }
+    */
 }
--- Assets/Script/SceneStartController.cs
+++ Assets/Script/SceneStartController.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneStartController : MonoBehaviour {
+    [SerializeField]
+    private bool isgamestart=false;
+    private bool isgameexit= false;
 
-    public void OnStartBtn()
+    /*public void OnStartBtn()
     {
         SceneManager.LoadScene("HomeMama");
     }
     public void OnExitBtn()
     {
         Application.Quit();
-    }
+    }*/
+
+    private void Update()
+    {
+        if (isgamestart)    //choose button start become true
+        {
+            SceneManager.LoadScene("HomeMama");
+        }
+        else if(isgameexit)     //choose button exit become true
+        {
+            Application.Quit();
+        }
+
+    } 
+        
+    
 }
