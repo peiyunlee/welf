8835cf45468b71f7d0ccf7043f9dec9098f2c234
Yue Chu Cheng <maggie9907@gmail.com>
2018-10-23 19:08:28 +0800

    10/23Yue Chu

---
+++ Assets/Script/Enemy/Enemymovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemymovement : MonoBehaviour {
+    public Transform Player;
+    private UnityEngine.AI.NavMeshAgent nav;
+
+
+    void Awake()
+    {
+        Player =GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+
+        nav.SetDestination(Player.position);
+        
+        
+
+
+
+    }
+}
