e491c5f01400d061139ceb42cc67a584f9fcf5e1
Yue Chu Cheng <maggie9907@gmail.com>
2018-10-23 22:16:25 +0800

    10/22YueChu

---
+++ Assets/Script/Enemy/EnemyHealth.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+    //遊戲開始時小怪的初始血量
+    public int startingHealth = 100;
+    //遊戲過程中記錄小怪血量
+    public int currentHealth;
+
+    public float sinkSpeed = 2.5f;
+    public int scoreValue = 10;
+    public AudioClip deathClip;
+
+
+    Animator anim;
+    AudioSource enemyAudio;
+    ParticleSystem hitParticles;
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+    bool isSinking;
+    // Use this for initialization
+
+    void Start () {
+        anim = GetComponent<Animator>();
+        enemyAudio = GetComponent<AudioSource>();
+        hitParticles = GetComponentInChildren<ParticleSystem>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+
+        currentHealth = startingHealth;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isSinking)
+        {
+            transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+    }
+    public void TakeDamage(int amount, Vector3 hitPoint)
+    {
+        if (isDead)
+            return;
+
+        enemyAudio.Play();
+
+        currentHealth -= amount;
+
+        hitParticles.transform.position = hitPoint;
+        hitParticles.Play();
+
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        isDead = true;
+
+        capsuleCollider.isTrigger = true;
+
+        anim.SetTrigger("Dead");
+
+        enemyAudio.clip = deathClip;
+        enemyAudio.Play();
+    }
+
+
+    /*public void StartSinking()
+    {
+        GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        isSinking = true;
+        ScoreManager.score += scoreValue;
+        Destroy(gameObject, 2f);
+    }*/
+}
