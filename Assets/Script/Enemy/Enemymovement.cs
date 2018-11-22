f855ce1eb86a4a7bcd6b79903ac20a77866519ab
Yue Chu Cheng <maggie9907@gmail.com>
2018-11-22 21:36:03 +0800

    YueChu

--- Assets/Script/Enemy/Enemymovement.cs
+++ Assets/Script/Enemy/Enemymovement.cs
@@ -3,19 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
     public class Enemymovement : MonoBehaviour {
-    public Transform main;
 
-    private float moveSpeed = 5.0f;
+    Rigidbody2D playerRigidbody;
+    public Transform main;//要跟随英雄
+    private CharacterController con;//怪物的角色控制器
+    
 
-    private float rotateSpeed = 5.0f;
+    public float followDis = 100f;//达到这个距离开始跟随
+    /*public float attackDis = 2f;//达到这个距离开始攻击
 
-    private Transform mytransform;//自己
-    Transform target = GameObject.FindGameObjectWithTag("main").transform;
+    public float thinkTime = 3f;//思考的时间
+    public float currentThinkTime = 0f;//记录思考的时间
 
+    public float walkTime = 5f;//走的时间
+    public float currentWalkTime = 0;//记录走的时间
+
+    private bool isAttact = false;//是否攻击
+    */
+    void Attack() {
+
+        playerRigidbody.velocity= new Vector2(3, 0);
+
+        if (Vector2.Distance(transform.position, main.position) <= followDis)//跟随距离
+        {
+           
+            Vector2 transformValue = new Vector2(main.position.x-transform.position.x-10, 0);
+            playerRigidbody.velocity = transformValue;
+
+        }
+    
+    }
 
 
     void Awake()
     {
+        playerRigidbody = GetComponent<Rigidbody2D>();
         Transform target = GameObject.FindGameObjectWithTag("main").transform;
 
     }
@@ -23,15 +45,8 @@ using UnityEngine;
 
     void Update()
     {
-        Debug.DrawLine(target.transform.position, this.transform.position, Color.yellow);
 
-
-        mytransform.rotation = Quaternion.Slerp(mytransform.rotation, Quaternion.LookRotation(target.position - mytransform.position), rotateSpeed * Time.deltaTime);
-
-
-        mytransform.position += mytransform.forward * moveSpeed * Time.deltaTime;
-
-
+        Attack();
 
 
     }
