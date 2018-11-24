4a414f072598f5a9eb9c1c5cafd717ad59f93cf9
May <may1092200258@gmail.com>
2018-11-25 01:42:55 +0800

    11/25 GU

--- Assets/StoryMenuController.cs
+++ Assets/StoryMenuController.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StoryMenuController : AllSceneController
+public class StoryMenuController : MonoBehaviour
 {
     public GameObject StoryMenu;
     public GameObject StoryMenuBtn;
     public GameObject ExitStoryimg;
     public GameObject OpenStoryimg;
+    [SerializeField]
+    private Vector3 vr0;
+    [SerializeField]
+    private Vector3 vr1;
     public float fhidespeed = 300f;
     [SerializeField]
     private bool isstorymenu = false;
+    [SerializeField]
+    private int count;
     enum SCENE
     {
         START = 0,
@@ -29,40 +35,45 @@ public class StoryMenuController : AllSceneController
     }
     void Start()
     {
-        
-        if (AllSceneController::iscenenumber < (int)SCENE.LaboratoryOne)
-        {
-            StoryMenuBtn.transform.position += new Vector3(-fhidespeed, 0f, 0f); //隱藏StoryMenuBtn
-            ExitStoryimg.transform.position += new Vector3(-fhidespeed, 0f, 0f); //隱藏ExitStoryimg
-            OpenStoryimg.transform.position += new Vector3(-fhidespeed, 0f, 0f); //隱藏OpenStoryimg
-            Debug.Log(AllSceneController.iscenenumber);
-        }
-        else
-        {
-            StoryMenuBtn.transform.position += new Vector3(0.0f, 0.0f, 0.0f); //顯示StoryMenuBtn
-            OpenStoryimg.transform.position += new Vector3(0.0f, 0.0f, 0.0f); //顯示OpenStoryimg
-            Debug.Log(2);
-        }
+        vr0= StoryMenuBtn.transform.position;
+        vr1 = StoryMenuBtn.transform.position + new Vector3(-fhidespeed, 0f, 0f);
+        StoryMenu.transform.position += new Vector3(-fhidespeed, 0.0f, 0.0f); //隱藏StoryMenu
+        StoryMenuBtn.transform.position = vr1; //隱藏StoryMenuBtn
+        ExitStoryimg.transform.position = vr1; //隱藏ExitStoryimg
+        OpenStoryimg.transform.position = vr1; //隱藏OpenStoryimg
     }
 
     void Update()
     {
+        if (AllSceneController.iscenenumber < (int)SCENE.LaboratoryOne)
+        {
+            StoryMenuBtn.transform.position = vr1; //隱藏StoryMenuBtn
+            ExitStoryimg.transform.position = vr1; //隱藏ExitStoryimg
+            OpenStoryimg.transform.position = vr1; //隱藏OpenStoryimg
+            Debug.Log(AllSceneController.iscenenumber);
+        }
+        else
+        {
+            StoryMenuBtn.transform.position = vr0; //顯示StoryMenuBtn
+            OpenStoryimg.transform.position = vr0; //顯示OpenStoryimg
+            Debug.Log(2);
+        }
     }
     public void OnStoryMenuBtnClick()
     {
         isstorymenu = !isstorymenu;
         if (isstorymenu)
         {
-            StoryMenu.transform.position += new Vector3(0.0f, 0.0f, 0.0f); //顯示StoryMenu
-            ExitStoryimg.transform.position += new Vector3(0.0f, 0.0f, 0.0f); //顯示ExitStoryimg
-            OpenStoryimg.transform.position += new Vector3(-fhidespeed, 0f, 0f); //隱藏OpenStoryimg
+            StoryMenu.transform.position += new Vector3(fhidespeed, 0.0f, 0.0f); //顯示StoryMenu
+            ExitStoryimg.transform.position = vr0; //顯示ExitStoryimg
+            OpenStoryimg.transform.position = vr1; //隱藏OpenStoryimg
             Debug.Log("1");
         }
         else
         {
-            StoryMenu.transform.position += new Vector3(0.0f, 0.0f, 0.0f); //顯示StoryMenu
-            OpenStoryimg.transform.position += new Vector3(0.0f, 0.0f, 0.0f); //顯示OpenStoryimg
-            ExitStoryimg.transform.position += new Vector3(-fhidespeed, 0f, 0f); //隱藏ExitStoryimg
+            StoryMenu.transform.position += new Vector3(-fhidespeed, 0.0f, 0.0f); //隱藏StoryMenu
+            OpenStoryimg.transform.position = vr0; //顯示OpenStoryimg
+            ExitStoryimg.transform.position = vr1; //隱藏ExitStoryimg
             Debug.Log("2");
         }
     }
