2e4560f946770aa4a7ad374a8b8483ef69acf06c
Yue Chu Cheng <maggie9907@gmail.com>
2018-12-02 14:22:13 +0800

    yue chu 12/2

--- Assets/Script/Enemy/EnemyHealth.cs
+++ Assets/Script/Enemy/EnemyHealth.cs
@@ -4,77 +4,82 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour {
     //遊戲開始時小怪的初始血量
-    public int startingHealth = 100;
+    public int startingHealth = 10;
     //遊戲過程中記錄小怪血量
     public int currentHealth;
-
-    public float sinkSpeed = 2.5f;
-    public int scoreValue = 10;
-    public AudioClip deathClip;
-
-
+    //怪物死亡數目
+    public int enemy_dead = 0;
+   
     Animator anim;
     AudioSource enemyAudio;
     ParticleSystem hitParticles;
     CapsuleCollider capsuleCollider;
-    bool isDead;
+    public bool isDead;
     bool isSinking;
+    bool bullet_tag=false;
     // Use this for initialization
 
+    //傷害
+    public void TakeDamage(int amount = 10)
+    {
+        Debug.Log("damaged");
+        if (isDead)
+            return;
+
+        currentHealth -= amount;
+        Debug.Log(currentHealth);
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+
+    }
+
+    //死亡
+    void Death()
+    {
+        Debug.Log("Dead");
+        isDead = true;
+        capsuleCollider.isTrigger = true;
+        anim.SetTrigger("Dead");
+        enemy_dead++;
+        
+    }
+
+
+
+
+    //偵測是否觸碰到子彈
+    void OnTriggerEnter2D(Collider2D border)
+    {
+
+        if (border.gameObject.CompareTag("bullet"))
+        {
+          
+            TakeDamage();
+
+        }
+    }
+
+
+
+
     void Start () {
-        anim = GetComponent<Animator>();
+        /*anim = GetComponent<Animator>();
         enemyAudio = GetComponent<AudioSource>();
         hitParticles = GetComponentInChildren<ParticleSystem>();
         capsuleCollider = GetComponent<CapsuleCollider>();
-
+        */
+       
         currentHealth = startingHealth;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (isSinking)
-        {
-            transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
-        }
+      
     }
-    public void TakeDamage(int amount, Vector3 hitPoint)
-    {
-        if (isDead)
-            return;
-
-        enemyAudio.Play();
-
-        currentHealth -= amount;
-
-        hitParticles.transform.position = hitPoint;
-        hitParticles.Play();
-
-        if (currentHealth <= 0)
-        {
-            Death();
-        }
-    }
+   
 
 
-    void Death()
-    {
-        isDead = true;
-
-        capsuleCollider.isTrigger = true;
-
-        anim.SetTrigger("Dead");
-
-        enemyAudio.clip = deathClip;
-        enemyAudio.Play();
-    }
-
-
-    /*public void StartSinking()
-    {
-        GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
-        GetComponent<Rigidbody>().isKinematic = true;
-        isSinking = true;
-        ScoreManager.score += scoreValue;
-        Destroy(gameObject, 2f);
-    }*/
+  
 }
--- Assets/Script/Enemy/Enemymovement.cs
+++ Assets/Script/Enemy/Enemymovement.cs
@@ -6,19 +6,15 @@ using UnityEngine;
 
     Rigidbody2D playerRigidbody;
     public Transform main;//要跟随英雄
-    public Transform enemyBorderRight;//要跟随英雄
-    public Transform enemyBorderLeft;
-    public Transform trackCharacterDistance;
-    private CharacterController con;//怪物的角色控制器
-    bool border_tag=false;
-    bool reset_border_tag = false;
-    bool enemy_long_tag = false;
-    bool border_left_tag = false;
 
-    public float followDis = 100f;//达到这个距离开始跟随
-    public float speed = 10f;
-    private float enemyposition;
-    public int enemyinitiallocat;
+
+    bool border_tag=false;//是否碰到限制範圍
+    public float followDis = 100f;//達到此距離開始攻擊
+    public float idle_speed = 10f;//移動速度
+    public float follow_speed = 15f;//跟隨速度
+    private EnemyHealth enemyHealth;
+
+    //判斷是否碰到所限制的範圍
     void OnTriggerEnter2D(Collider2D border)
     {
        
@@ -28,54 +24,51 @@ using UnityEngine;
 
 
         }
-     /*   if (border.gameObject.CompareTag("reset_border"))
-        {
-            reset_border_tag = true;
+     }
 
-
-        }*/
-
-        /* if (border.gameObject.CompareTag("borderleft"))
-         {
-             border_left_tag = true;
-
-
-         }*/
-
-    }
-
+    //跟隨函式
     void follow()
     {
-
-
-
-     
-       
-
-
+        //當於怪物的右側
             if (Vector2.Distance(transform.position, main.position) <= followDis && (main.position.x - transform.position.x > 0))//跟随距离
             {
 
                 Vector2 transformValue = new Vector2(10, 0);
-
-
                 playerRigidbody.velocity = transformValue;
 
             }
 
+        //當於怪物的左側
 
-
-            else if (Vector2.Distance(transform.position, main.position) <= followDis && (main.position.x - transform.position.x < 0))//跟随距离
+        if (Vector2.Distance(transform.position, main.position) <= followDis && (main.position.x - transform.position.x < 0))//跟随距离
             {
 
                 Vector2 transformValue = new Vector2(-10, 0);
-
                 playerRigidbody.velocity = transformValue;
 
             }
+    }
 
 
-        
+    //怪物idle狀態
+
+    void idle()
+    {
+
+
+        Vector2 transformValue = new Vector2(idle_speed, 0);
+
+        playerRigidbody.velocity = transformValue;
+        if (border_tag == true)
+        {
+
+            idle_speed = idle_speed * -1;
+
+            border_tag = false;
+        }
+
+
+
 
     }
 
@@ -84,53 +77,40 @@ using UnityEngine;
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
         Transform target = GameObject.FindGameObjectWithTag("main").transform;
-
+        enemyHealth = GetComponent<EnemyHealth>();
     }
     
    
     
 
-    void move()
-    {
-
-        /*this.gameObject.transform.position += new Vector3(speed, 0f, 0f);*/
-        Vector2 transformValue = new Vector2(speed, 0);
-
-        playerRigidbody.velocity = transformValue;
-        if (border_tag==true)
-        {
-          
-            speed = speed * -1;
-
-            border_tag = false;
-        }        
-
-
-        
-        
-    }
+   
   
     void Update()
     {
-       
-    
 
-        enemyposition = this.gameObject.transform.position.x;
-    
-
-        if ((Vector2.Distance(transform.position, main.position) <= followDis && (main.position.x - transform.position.x > 0)))
+        
+        if (enemyHealth.isDead)
         {
-            follow();
-           
-        }
-        else
-        {
-            move();
-
+            Vector2 transformValue = new Vector2(0, 0);
+            playerRigidbody.velocity = transformValue;
 
         }
-       
+        else {
+            if (Vector2.Distance(transform.position, main.position) <= followDis)
+            {
+                follow();
+
+            }
+            else
+            {
+                idle();
+
+
+            }
+
+        }
+
     }
    
-   }
+}
 
