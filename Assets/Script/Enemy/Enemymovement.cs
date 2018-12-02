c43641c5e496c628770a7ad8bb3a3ca7c2252764
Yue Chu Cheng <maggie9907@gmail.com>
2018-12-02 11:55:58 +0800

    yue chu 12/2

--- Assets/Script/Enemy/Enemymovement.cs
+++ Assets/Script/Enemy/Enemymovement.cs
@@ -10,78 +10,75 @@ using UnityEngine;
     public Transform enemyBorderLeft;
     public Transform trackCharacterDistance;
     private CharacterController con;//怪物的角色控制器
-    bool border_right_tag=false;
+    bool border_tag=false;
+    bool reset_border_tag = false;
     bool enemy_long_tag = false;
     bool border_left_tag = false;
 
     public float followDis = 100f;//达到这个距离开始跟随
-    public float speed = 0.5f;
+    public float speed = 10f;
     private float enemyposition;
     public int enemyinitiallocat;
     void OnTriggerEnter2D(Collider2D border)
     {
-        Debug.Log("sdfsdf");
+       
         if (border.gameObject.CompareTag("border"))
         {
-            border_right_tag = true;
+            border_tag = true;
 
 
         }
-        if (border.gameObject.CompareTag("enemylongborder"))
+     /*   if (border.gameObject.CompareTag("reset_border"))
         {
-            enemy_long_tag = true;
+            reset_border_tag = true;
 
 
-        }
-        if (border.gameObject.CompareTag("borderleft"))
-        {
-            border_left_tag = true;
+        }*/
+
+        /* if (border.gameObject.CompareTag("borderleft"))
+         {
+             border_left_tag = true;
 
 
-        }
+         }*/
 
     }
-    
+
     void follow()
     {
 
-        /* playerRigidbody.velocity= new Vector2(3, 0);*/
-
-        if (Vector2.Distance(transform.position, main.position) <= followDis && (main.position.x - transform.position.x > 0))//跟随距离
-        {
-
-            Vector2 transformValue = new Vector2(10, 0);
 
 
-            playerRigidbody.velocity = transformValue;
-
-        }
-
+     
+       
 
 
-        if (Vector2.Distance(transform.position, main.position) <= followDis && (main.position.x - transform.position.x < 0))//跟随距离
-        {
-
-            Vector2 transformValue = new Vector2(-10, 0);
-
-            playerRigidbody.velocity = transformValue;
-
-        }
-        if (enemy_long_tag == true)
-        {
-            Debug.Log("20");
-            while (border_left_tag!=true)
+            if (Vector2.Distance(transform.position, main.position) <= followDis && (main.position.x - transform.position.x > 0))//跟随距离
             {
-                Vector2 transformValue = new Vector2(-10, 0);
+
+                Vector2 transformValue = new Vector2(10, 0);
+
+
+                playerRigidbody.velocity = transformValue;
 
             }
-            enemy_long_tag = false;
-            border_left_tag = false;
 
 
-        }
+
+            else if (Vector2.Distance(transform.position, main.position) <= followDis && (main.position.x - transform.position.x < 0))//跟随距离
+            {
+
+                Vector2 transformValue = new Vector2(-10, 0);
+
+                playerRigidbody.velocity = transformValue;
+
+            }
+
+
+        
 
     }
+
 
     void Awake()
     {
@@ -91,43 +88,37 @@ using UnityEngine;
     }
     
    
-    public int enemylong=20;
+    
 
     void move()
     {
-        
-        this.gameObject.transform.position += new Vector3(speed, 0f, 0f);
-        if (border_right_tag==true)
+
+        /*this.gameObject.transform.position += new Vector3(speed, 0f, 0f);*/
+        Vector2 transformValue = new Vector2(speed, 0);
+
+        playerRigidbody.velocity = transformValue;
+        if (border_tag==true)
         {
-            /*Debug.Log("1");*/
+          
             speed = speed * -1;
 
-            border_right_tag = false;
+            border_tag = false;
         }        
 
 
         
         
     }
-   /* void reset()
-    {
-
-
-
-
-    }*/
+  
     void Update()
     {
-        /*Debug.Log(speed);
-        Debug.Log(enemyposition);*/
+       
+    
 
         enemyposition = this.gameObject.transform.position.x;
-        /*if (enemy_long_tag == true)
-        {
-            reset();
-        }*/
+    
 
-        if ((Vector2.Distance(transform.position, main.position) <= followDis && (main.position.x - transform.position.x > 0))&&(enemyposition<8))
+        if ((Vector2.Distance(transform.position, main.position) <= followDis && (main.position.x - transform.position.x > 0)))
         {
             follow();
            
