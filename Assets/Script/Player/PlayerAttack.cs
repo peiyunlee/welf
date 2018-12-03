0280ff4b8a6ace720a7041f9c194a1929be5aee8
wendy556609 <wendy556609@gmail.com>
2018-12-04 00:38:28 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/PEIYUNLEE/TeamGame""
    This reverts commit 673350350761b1b08b222d1cbcda6d59e223d537.

--- Assets/Script/Player/PlayerAttack.cs
+++ Assets/Script/Player/PlayerAttack.cs
@@ -3,77 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
-    enum Attack
-    {
-        normal,
-        skill
-    }
-    Attack playerAttack;
-
-    int attackCount = 0;
-    bool keyAttack;
-    bool isAttack = false;
-
-    //動畫名稱
-    private const string idleState = "Idle";
-    private const string attack1State = "Attack1";
-    private const string attack2State = "Attack2";
-    PlayerMovement playerMovement;
+    bool attack;
     Animator playerAnim;
-    AnimatorStateInfo animSta;
     // Use this for initialization
     void Start () {
         playerAnim = GetComponent<Animator>();
-    }
+	}
 	
 	// Update is called once per frame
 	void Update () {
         GetKey();
 
-        Animating();
-
-        SetAttack();
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        
+        //Animating()
     }
 
-    void SetAttack()
+    void Attack()
     {
-        animSta = playerAnim.GetCurrentAnimatorStateInfo(0);
-        if (!animSta.IsName(idleState)&&animSta.normalizedTime>1.0f)
-        {
-            attackCount = 0;
-            playerAnim.SetInteger("Attack", attackCount);
-        }
-        if (keyAttack)
-        {
-            if (animSta.IsName(idleState) && attackCount == 0)
-            {
-                attackCount = 1;
-                playerAnim.SetInteger("Attack", attackCount);
-            }
-            if (animSta.IsName(attack1State) && attackCount == 1 && animSta.normalizedTime > 0.5f)
-            {
-                attackCount = 2;
-                playerAnim.SetInteger("Attack", attackCount);
-            }
-            //if (attackCount == 2)
-            //{
-            //    attackCount = 0;
-            //}
-        }
-       // isAttack = false;
+
     }
 
     void GetKey()
     {
-        keyAttack = Input.GetButtonDown("Attack");       
+        attack = Input.GetButtonDown("Attack");
     }
 
-    void Animating()
-    {
-
-    }
+    //void Animating()
+    //{
+    //    if()
+    //}
 }
