a06f987f934ebb79172d229dd73fbec771e75dcd
wendy556609 <wendy556609@gmail.com>
2018-12-04 00:22:26 +0800

    Revert "12/04 WunJhu"
    This reverts commit c9b1d17954fa620f41b9b160f14cf7d15155bb67.

--- Assets/Script/Player/PlayerAttack.cs
+++ Assets/Script/Player/PlayerAttack.cs
@@ -3,68 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
-    public enum Attack
+    enum Attack
     {
-        idle,
         normal,
         skill
     }
+    Attack playerAttack;
 
-    int hurt = 0;
     int attackCount = 0;
     bool keyAttack;
     bool isAttack = false;
 
     //動畫名稱
     private const string idleState = "Idle";
-    private const string runState = "Run1";
     private const string attack1State = "Attack1";
     private const string attack2State = "Attack2";
-
-    GameObject attackRange;
-
-    SetAttack setAttack;
     PlayerMovement playerMovement;
     Animator playerAnim;
     AnimatorStateInfo animSta;
     // Use this for initialization
-    void Start() {
+    void Start () {
         playerAnim = GetComponent<Animator>();
-        setAttack = GetComponent<SetAttack>();
-        Debug.Log("pa");
-        attackRange = GameObject.FindGameObjectWithTag("playerAttack");
-
-        // attackRange.SetActive(false);
     }
-
-    // Update is called once per frame
-    void Update() {
+	
+	// Update is called once per frame
+	void Update () {
         GetKey();
 
-        AttackSet();
+        Animating();
+
+        SetAttack();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
     }
 
-    void AttackSet()
+    void SetAttack()
     {
         animSta = playerAnim.GetCurrentAnimatorStateInfo(0);
-        if (!animSta.IsName(idleState) && animSta.normalizedTime > 1.0f)
+        if (!animSta.IsName(idleState)&&animSta.normalizedTime>1.0f)
         {
             attackCount = 0;
             playerAnim.SetInteger("Attack", attackCount);
-            //attackRange.SetActive(false);
         }
         if (keyAttack)
         {
-            if ((animSta.IsName(idleState)|| animSta.IsName(runState)) && attackCount == 0)
+            if (animSta.IsName(idleState) && attackCount == 0)
             {
-                // attackRange.SetActive(true);
-               
                 attackCount = 1;
                 playerAnim.SetInteger("Attack", attackCount);
             }
             if (animSta.IsName(attack1State) && attackCount == 1 && animSta.normalizedTime > 0.5f)
             {
-                //attackRange.SetActive(true);
                 attackCount = 2;
                 playerAnim.SetInteger("Attack", attackCount);
             }
@@ -73,13 +64,16 @@ public class PlayerAttack : MonoBehaviour {
             //    attackCount = 0;
             //}
         }
-        // isAttack = false;
+       // isAttack = false;
     }
 
-
-
-        void GetKey()
-        {
-            keyAttack = Input.GetButtonDown("Attack");
-        }
+    void GetKey()
+    {
+        keyAttack = Input.GetButtonDown("Attack");       
     }
+
+    void Animating()
+    {
+
+    }
+}
