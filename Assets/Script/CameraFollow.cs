d3c9444bca34e907138cc290f808b06747616c4d
May <may1092200258@gmail.com>
2018-12-05 00:57:11 +0800

    12/5 camera gu

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -6,17 +6,12 @@ public class CameraFollow : MonoBehaviour {
     // 這個程式會附加到撥放器main Camera內，這裡死區deadZone設置為0，可以在unity內調到想要的效果
     [SerializeField]
     private GameObject target;
-
     [SerializeField]
-    private float deadZone = 0;
+    public float addy;
     [SerializeField]
-    private bool followVertical = true;
+    public float miny;
     [SerializeField]
-    private bool followHorizontal = true;
-    [SerializeField]
-    private float addy;
-    [SerializeField]
-    private float miny;
+    public float jumphigh;
     [SerializeField]
     private float maxvector;
     [SerializeField]
@@ -29,41 +24,41 @@ public class CameraFollow : MonoBehaviour {
 
         if (target.transform.position.x < minvector)
         {
-            if (target.transform.position.y < 0.164999f)
+            if (target.transform.position.y < jumphigh)
             {
                 transform.position = new Vector3(minvector, miny, transform.position.z);
                 Debug.Log("a");
             }
             else
             {
-                transform.position = new Vector3(minvector, target.transform.position.y, transform.position.z);
+                transform.position = new Vector3(minvector, target.transform.position.y + addy, transform.position.z);
                 Debug.Log("b");
             }
         }
         else if (target.transform.position.x > maxvector)
         {
-            if (target.transform.position.y < 0.164999f)
+            if (target.transform.position.y < jumphigh)
             {
                 transform.position = new Vector3(maxvector, miny, transform.position.z);
                 Debug.Log("c");
             }
             else
             {
-                transform.position = new Vector3(maxvector, transform.position.y, transform.position.z);
+                transform.position = new Vector3(maxvector, target.transform.position.y + addy, transform.position.z);
                 Debug.Log("d");
             }
         
     }
         else
         {
-            if (target.transform.position.y < 0.164999f)
+            if (target.transform.position.y < jumphigh)
             {
                 transform.position = new Vector3(target.transform.position.x, miny, transform.position.z);
                 Debug.Log("e");
             }
             else
             { 
-                transform.position = new Vector3(target.transform.position.x, target.transform.position.y + addy, transform.position.z);
+                transform.position = new Vector3(target.transform.position.x, target.transform.position.y+addy, transform.position.z);
                 Debug.Log("f");
             }
         }
