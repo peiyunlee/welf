bc5fbeceb3b3104f3775732d069796b9648bbb72
Yue Chu Cheng <maggie9907@gmail.com>
2018-12-08 12:53:20 +0800

    yue-chu 12/8

---
+++ Assets/Script/Enemy/middleBoss/middleEnemy_Attack_bighit/leftBulletController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leftBulletController : MonoBehaviour {
+    public float rate = 3; //發子彈的速率
+    public GameObject bullet;//取得子彈的gameobject
+    private int bullet_time = 0;
+    float timer;
+    // Use this for initialization
+    void Start () {
+
+        fire();
+
+
+    }
+   
+	
+	public void fire()
+    {
+        GameObject.Instantiate(bullet,transform.position,Quaternion.identity);//複製子彈
+      
+    }
+    
+
+    
+}
---
+++ Assets/Script/Enemy/middleBoss/middleEnemy_Attack_shock.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class middleEnemy_Attack_shock : MonoBehaviour {
+
+    Rigidbody2D playerRigidbody;
+    public Transform main;//要跟随英雄
+
+
+
+
+
+
+    private void normalAttack_hit()
+    {
+
+
+        if (((transform.position.x - initialposition) > 20))
+        {
+
+            Vector2 transformValue = new Vector2(0, 0);
+            playerRigidbody.velocity = transformValue;
+            //Debug.Log("pause");
+            System.Threading.Thread.Sleep(500);
+            transform.Translate(new Vector2(initialposition, 0) * 1f);
+        }
+        else
+        {
+
+            transform.Translate(new Vector2(5, 0) * 0.5f);
+
+        }
+
+
+
+    }
+
+    private float initialposition;
+    private void Start()
+    {
+        initialposition = transform.position.x;
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        Transform target = GameObject.FindGameObjectWithTag("main").transform;
+
+    }
+
+
+    private void Update()
+    {
+
+        normalAttack_hit();
+
+
+
+
+    }
+
+}
