f0316a86424e57267cfeb336ed6380eabc41ba38
May <may1092200258@gmail.com>
2018-12-14 13:39:16 +0800

    12/14

---
+++ Assets/Script/Scene/DadScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DadScene : MonoBehaviour {
+    [SerializeField]
+    private Fungus.Flowchart flowchart;
+    [SerializeField]
+    private GameObject []target;
+    void Update()
+    {
+        if (flowchart.GetBooleanVariable("isplayer") == true)
+        {
+            PlayerMovement.isMenu = false;
+        }
+        else
+        {
+            PlayerMovement.isMenu = true;
+        }
+        if (flowchart.GetBooleanVariable("isstart") == true)
+        {
+            target[0].GetComponent<BoxCollider2D>().isTrigger = false;
+            target[1].GetComponent<BoxCollider2D>().isTrigger = false;
+        }
+        if (flowchart.GetBooleanVariable("isend") == true)
+        {
+            target[1].GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+    }
+}
