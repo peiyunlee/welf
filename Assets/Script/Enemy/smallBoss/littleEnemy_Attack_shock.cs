69cba652dc97a44fd8fea4f683652c9deafa43b8
Yue Chu Cheng <maggie9907@gmail.com>
2018-12-19 23:24:26 +0800

    yue-chu 12/19

--- Assets/Script/Enemy/smallBoss/littleEnemy_Attack_shock.cs
+++ Assets/Script/Enemy/smallBoss/littleEnemy_Attack_shock.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class littleEnemy_Attack_shock : MonoBehaviour {
+   // private Enemymovement enemymovement;
     Rigidbody2D playerRigidbody;
     public Transform main;//要跟随英雄
     private EnemyAttack_manager EnemyAttack_manager;
     public bool state = false;
     float speed = 100f;//衝擊速度
     float shockRange = 10f;
-    float timer = 0;
-    bool overState = false;
+    public float timer = 0;
+    public bool State_right = false;
+    public bool State_left = false;
     void refresh()
     {
 
@@ -21,27 +23,29 @@ public class littleEnemy_Attack_shock : MonoBehaviour {
     }
 
 
-    private void normalAttack_hit()
+    public void normalAttack_hit_right()
     {
+       State_right = true;
         timer += Time.deltaTime;
-
+       
         if (timer >= 0.5f && timer <= 1.5f)//暫停時間
         {
             Vector2 transformValue = new Vector2(0, 0);
-            playerRigidbody.velocity = transformValue;
+           // playerRigidbody.velocity = transformValue;
 
         }
         else if (timer >= 1.5f && timer <= 2f)//返回時間
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            /* transform.Translate(Vector3.left * speed * Time.deltaTime);*/
+            State_right = false;
 
         }
-        else if (timer >= 2f)
+       /* else if (timer >= 2f)
         {
 
-            overState = true;
+            State_right = false;
 
-        }
+        }*/
         else
         {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
@@ -53,13 +57,49 @@ public class littleEnemy_Attack_shock : MonoBehaviour {
 
     }
 
+
+    public void normalAttack_hit_left()
+    {
+        timer += Time.deltaTime;
+        State_left = true;
+        if (timer >= 0.5f && timer <= 1.5f)//暫停時間
+        {
+            Vector2 transformValue = new Vector2(0, 0);
+            // playerRigidbody.velocity = transformValue;
+
+        }
+        else if (timer >= 1.5f && timer <= 2f)//返回時間
+        {
+           /* transform.Translate(Vector3.right * speed * Time.deltaTime);*/
+            State_left = false;
+
+        }
+       /* else if (timer >= 2f)
+        {
+
+            State_left = false;
+
+        }*/
+        else
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+
+        }
+
+
+
+    }
+
+
     private float initialposition;
     private void Start()
     {
         initialposition = transform.position.x;
         playerRigidbody = GetComponent<Rigidbody2D>();
         Transform target = GameObject.FindGameObjectWithTag("main").transform;
-        EnemyAttack_manager = GetComponent<EnemyAttack_manager>(); //與外部判斷是否fire做連結
+        //EnemyAttack_manager = GetComponent<EnemyAttack_manager>(); //與外部判斷是否fire做連結
+      //  enemymovement = GetComponent<Enemymovement>();
     }
 
 
@@ -67,10 +107,10 @@ public class littleEnemy_Attack_shock : MonoBehaviour {
     {
 
         Debug.Log(EnemyAttack_manager.shockhit_state);
-        if (overState == false && EnemyAttack_manager.shockhit_state == true)
+        if (State_right == false && EnemyAttack_manager.shockhit_state == true)
         {
 
-            normalAttack_hit();
+            normalAttack_hit_right();
             state = true;
         }
 
