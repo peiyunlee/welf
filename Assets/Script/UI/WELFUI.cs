c4b44e8ce8814ce2823dc1b3842fd419e6a60e02
May <may1092200258@gmail.com>
2018-12-21 11:47:58 +0800

    12/21

--- Assets/Script/UI/WELFUI.cs
+++ Assets/Script/UI/WELFUI.cs
@@ -5,60 +5,32 @@ using UnityEngine.UI;
 
 public class WELFUI : MonoBehaviour {
     [SerializeField]
+    GameObject [] gameobject;
+    [SerializeField]
     Image [] myImage;
     [SerializeField]
+    Animator [] anim;
+    [SerializeField]
     private int [] mychooseelf;
+    [SerializeField]
+    public int whitchelf;
     // Use this for initialization
     void Start () {
+        anim[0]=gameobject[0].GetComponent<Animator>();
+        anim[1] = gameobject[1].GetComponent<Animator>();
         mychooseelf[0] = GameManager.chooseelf[0];
         mychooseelf[1] = GameManager.chooseelf[1];
         myImage[0].sprite = Resources.Load("Images/UI/遊戲頭像/" + mychooseelf[0] + "_C", typeof(Sprite)) as Sprite;
         myImage[1].sprite = Resources.Load("Images/UI/遊戲頭像/" + mychooseelf[1] + "_H", typeof(Sprite)) as Sprite;
         myImage[2].sprite = Resources.Load("Images/UI/遊戲頭像/" + mychooseelf[0] + "_CS", typeof(Sprite)) as Sprite;
         myImage[3].sprite = Resources.Load("Images/UI/遊戲頭像/ALL_N", typeof(Sprite)) as Sprite;
-        //switch (mychooseelf[0])
-        //{
-        //    case 1:
-        //        myImage[0].sprite = Resources.Load("Images/UI/遊戲頭像/"+ mychooseelf[0] + "_C", typeof(Sprite)) as Sprite;
-        //        switch (mychooseelf[1])
-        //        {
-        //            case 2:
-        //                myImage[1].sprite = Resources.Load("Images/UI/遊戲頭像/急性子_C", typeof(Sprite)) as Sprite;
-        //                break;
-        //            case 3:
-        //                myImage[1].sprite = Resources.Load("Images/UI/遊戲頭像/溫柔_C", typeof(Sprite)) as Sprite;
-        //                break;
-        //        }
-        //        break;
-        //    case 2:
-        //        myImage[0].sprite = Resources.Load("Images/UI/遊戲頭像/急性子_C", typeof(Sprite)) as Sprite;
-        //        switch (mychooseelf[1])
-        //        {
-        //            case 1:
-        //                myImage[1].sprite = Resources.Load("Images/UI/遊戲頭像/大力_C", typeof(Sprite)) as Sprite;
-        //                break;
-        //            case 3:
-        //                myImage[1].sprite = Resources.Load("Images/UI/遊戲頭像/溫柔_C", typeof(Sprite)) as Sprite;
-        //                break;
-        //        }
-        //        break;
-        //    case 3:
-        //        myImage[0].sprite = Resources.Load("Images/UI/遊戲頭像/溫柔_C", typeof(Sprite)) as Sprite;
-        //        switch (mychooseelf[1])
-        //        {
-        //            case 2:
-        //                myImage[1].sprite = Resources.Load("Images/UI/遊戲頭像/急性子_C", typeof(Sprite)) as Sprite;
-        //                break;
-        //            case 1:
-        //                myImage[1].sprite = Resources.Load("Images/UI/遊戲頭像/大力_C", typeof(Sprite)) as Sprite;
-        //                break;
-        //        }
-        //        break;
-        //}
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (whitchelf == 1)
+        {
+            gameobject[0].anim.SetTrigger();
+        }
 	}
 }
