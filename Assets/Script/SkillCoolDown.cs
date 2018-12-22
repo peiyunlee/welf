5e927f298c7510643e94f140b493ff1bedae3240
May <may1092200258@gmail.com>
2018-12-22 20:10:44 +0800

    12/22

---
+++ Assets/Script/SkillCoolDown.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillCoolDown : MonoBehaviour {
+
+    // 技能的图标
+    public Image [] icon;
+    // 技能的冷却时间
+    public float coolDown;
+    // 保存当前技能的冷却时间
+    [SerializeField]
+    private float [] currentCoolDown;
+    public bool [] testbool;         //test
+    public bool[] testcanuseskill;
+    public void UseSkill(int whitchelf)
+    {
+        if (currentCoolDown[whitchelf-1] >= coolDown)
+        {
+            testcanuseskill[whitchelf - 1] = false;
+            // 重置冷却时间
+            currentCoolDown[whitchelf - 1] = 0;
+        }
+    }
+    void Start()
+    {
+        currentCoolDown = new float[2];
+        testcanuseskill = new bool[2];   //test
+        for (int i = 0; i < 2; i++)
+        {
+            currentCoolDown[i] = 10;
+            testcanuseskill[i] = true;
+        }
+        UseSkill(1);
+    }
+    void Update()
+    {
+        /////////////////////////////////
+        //if (testbool[0])
+        //{
+        //    UseSkill(1);
+        //    testbool[0] = false;
+        //}
+        //else if (testbool[1])
+        //{
+        //    UseSkill(2);
+        //    testbool[1] = false;
+        //}
+        //////////////////////////////////////
+        for (int i = 0; i < 2; i++)
+        {
+            switch (testcanuseskill[i])
+            {
+                case false:
+                    if (currentCoolDown[i] < coolDown)
+                    {
+                        // 更新冷却
+                        currentCoolDown[i] += Time.deltaTime;
+                        // 显示冷却动画
+                        icon[i].fillAmount = (coolDown - currentCoolDown[i]) / coolDown;
+                    }
+                    if (currentCoolDown[i] > coolDown)
+                    {
+                        testcanuseskill[i] = true;
+                    }
+                    break;
+                case true:
+                    break;
+
+            }
+            
+        }
+        
+    }
+}
--- Assets/Script/UI/WELFUI.cs
+++ Assets/Script/UI/WELFUI.cs
@@ -13,24 +13,49 @@ public class WELFUI : MonoBehaviour {
     [SerializeField]
     private int [] mychooseelf;
     [SerializeField]
-    public int whitchelf;
+    public int whitchelftest;  //test player use whitchelf
+    [SerializeField]
+    public bool ischangetest;  //test
     // Use this for initialization
     void Start () {
         anim[0]=gameobject[0].GetComponent<Animator>();
         anim[1] = gameobject[1].GetComponent<Animator>();
-        mychooseelf[0] = GameManager.chooseelf[0];
-        mychooseelf[1] = GameManager.chooseelf[1];
-        myImage[0].sprite = Resources.Load("Images/UI/遊戲頭像/" + mychooseelf[0] + "_C", typeof(Sprite)) as Sprite;
-        myImage[1].sprite = Resources.Load("Images/UI/遊戲頭像/" + mychooseelf[1] + "_H", typeof(Sprite)) as Sprite;
-        myImage[2].sprite = Resources.Load("Images/UI/遊戲頭像/" + mychooseelf[0] + "_CS", typeof(Sprite)) as Sprite;
-        myImage[3].sprite = Resources.Load("Images/UI/遊戲頭像/ALL_N", typeof(Sprite)) as Sprite;
+        mychooseelf[0] = 3;
+        mychooseelf[1] = 2;
+        ischangetest = true;
+        whitchelftest = 1;
+        anim[0].SetTrigger(mychooseelf[0] + "tobig");
+        anim[1].SetTrigger(mychooseelf[1] + "tosmall");
+        //myImage[0].sprite = Resources.Load("Images/UI/遊戲頭像/" + mychooseelf[0] + "_C", typeof(Sprite)) as Sprite;
+        //myImage[1].sprite = Resources.Load("Images/UI/遊戲頭像/" + mychooseelf[1] + "_H", typeof(Sprite)) as Sprite;
+        //myImage[2].sprite = Resources.Load("Images/UI/遊戲頭像/" + mychooseelf[0] + "_CS", typeof(Sprite)) as Sprite;
+        //myImage[3].sprite = Resources.Load("Images/UI/遊戲頭像/ALL_N", typeof(Sprite)) as Sprite;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (ischangetest)
+        {
+            ExcuteAnime(ref whitchelftest);
+            ischangetest = false;
+        }
+        
+    }
+    void ExcuteAnime(ref int whitchelf)
+    {
         if (whitchelf == 1)
         {
-            anim[0].SetTrigger("");
+            anim[0].SetTrigger(mychooseelf[0] + "tobig");
+            anim[1].SetTrigger(mychooseelf[1] + "tosmall");
+            whitchelf = 2;
+            Debug.Log("1");
         }
-	}
+        else if (whitchelf == 2)
+        {
+            anim[0].SetTrigger(mychooseelf[0] + "tosmall");
+            anim[1].SetTrigger(mychooseelf[1] + "tobig");
+            whitchelf = 1;
+            Debug.Log("2");
+        }
+    }
 }
