1a2150e53acb0c0f6fcf1643df38f15aefc2ced3
May <may1092200258@gmail.com>
2018-12-24 02:12:32 +0800

    12/24

---
+++ Assets/Script/UI/StoryCharacter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryCharacter : MonoBehaviour {
+    public GameObject[] story;
+    public Image[] image;
+    private int currentstorynumber;
+	// Use this for initialization
+	void Start () {
+        currentstorynumber = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnCharacterBtnClick(int stroynumber)
+    {
+        story[currentstorynumber - 1].SetActive(false);
+        story[stroynumber - 1].SetActive(true);
+        image[currentstorynumber - 1].sprite = Resources.Load("Images/UI/StoryMenu/" + currentstorynumber + "_N", typeof(Sprite)) as Sprite;
+        image[stroynumber - 1].sprite = Resources.Load("Images/UI/StoryMenu/" + stroynumber + "_H", typeof(Sprite)) as Sprite;
+        currentstorynumber = stroynumber;
+    }
+}
