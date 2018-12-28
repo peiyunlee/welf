61c4008b208a5ce5535473327c46003d0275080b
May <may1092200258@gmail.com>
2018-12-28 16:24:26 +0800

    12/28

--- Assets/Script/UI/Cloudmove.cs
+++ Assets/Script/UI/Cloudmove.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Cloudmove : MonoBehaviour {
-    GameObject currentcloud;
-    GameObject nextcloud;
+    public float fstartx;
+    float fturnx;
+    bool hasinstantiate;
+    Vector3 parentposition;
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        gameObject.transform.position -=new Vector3( 0.1f,0f,0f);
+        hasinstantiate = false;
+        fturnx = -205f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        gameObject.transform.position -=new Vector3( 0.05f,0f,0f);
+        Debug.Log(gameObject.transform.position);
+        if (gameObject.transform.position.x < fturnx && !hasinstantiate)
+        {
+            hasinstantiate = true;
+            gameObject.transform.position= new Vector3(fstartx, -0.3f, 69.7f);
+            Debug.Log("aa");
+            hasinstantiate = false;
+        }
 	}
 }
